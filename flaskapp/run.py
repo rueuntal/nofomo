ae4a34976ad2664c462f9c25f9f548ee22608121
Xiao Xiao <xiaoxiao@Xiaos-MacBook-Air.local>
2017-06-26 22:07:55 -0400

    Play with run to try to get worker started.

--- flaskapp/run.py
+++ flaskapp/run.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 from flaskexample import app
 app.run(host = '0.0.0.0', debug = True)
+
+from celery import current_app
+from celery.bin import worker
+
+application = current_app._get_current_object()
+
+worker = worker.worker(app=application)
+
+options = {
+    'broker': app.config['redis://localhost:6379/0'],
+    'loglevel': 'INFO',
+    'traceback': True,
+}
+
+worker.run(**options)
