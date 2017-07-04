ed49b9a11f1d5528ac77122b5436a6c66e89b4b7
Xiao Xiao <xiaoxiao@Xiaos-MacBook-Air.local>
2017-07-04 19:59:28 -0400

    Fix bug.

--- flaskapp/flaskexample/tweet_functions.py
+++ flaskapp/flaskexample/tweet_functions.py
@@ -313,5 +313,5 @@ def plot_Ntweets(tweet_count, peak_time, peak_vals, hashtag, start_time):
 def warning_func(hashtag, start_time):
     "If max(tweet_count) or mean(tweet_count) do not reach threshold, record input to file."
     warning_file = open('flaskexample/static/warning_cases.txt', 'a')
-    print warning_file, hashtag.strip('#') + '\t' + start_time
+    print >> warning_file, hashtag.strip('#') + '\t' + start_time
     warning_file.close()
